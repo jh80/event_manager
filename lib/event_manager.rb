4c479ff71f48056b7f3de93b3d66451c44cfc9e9
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-10-28 11:15:05 -0700

    Find weekday most registered on

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'pry-byebug'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -46,7 +47,7 @@ def clean_phone(phone_string)
   return "No valid phone number on file"
 end
 
-def hifreq_reg_hours_from_hash(frequency_hash)
+def highest_freq_from_hash(frequency_hash)
   frequency_hash.reduce({max: 0, hours: []}) do |max_hash, (hour, frequency)|
     unless max_hash[:max] > frequency
       if max_hash[:max] == frequency
@@ -60,10 +61,10 @@ def hifreq_reg_hours_from_hash(frequency_hash)
   end
 end
 
-def frequency_hash(attendees)
+def frequency_hash(attendees, stat)
   attendees.each_with_object(Hash.new) do |attendee, times_frequency|
-  reg_hour = attendee[:reg_time].hour
-  times_frequency[reg_hour] = times_frequency[reg_hour] ? times_frequency[reg_hour] + 1 : 1
+  attendee_stat = attendee[stat]
+  times_frequency[attendee_stat] = times_frequency[attendee_stat] ? times_frequency[attendee_stat] + 1 : 1
   end
 end
 
@@ -89,9 +90,14 @@ attendees = contents.map do |row|
 
   reg_time = Time.strptime(row[1], '%D %R')
 
+  reg_hour = reg_time.hour
+
+  reg_wday = Date::DAYNAMES[Date.strptime(row[1], '%D %R').wday]
+
   #save_thank_you_letter(id, form_letter)  
   
-  {id: id, name: name, zipcode: zipcode, legislators: legislators, phone: phone, reg_time: reg_time}
+  {id: id, name: name, zipcode: zipcode, legislators: legislators, phone: phone, reg_time: reg_time, reg_hour: reg_hour, reg_wday: reg_wday}
 end
 
-puts hifreq_reg_hours_from_hash(frequency_hash(attendees))
+puts highest_freq_from_hash(frequency_hash(attendees, :reg_wday))
+puts highest_freq_from_hash(frequency_hash(attendees, :reg_hour))
